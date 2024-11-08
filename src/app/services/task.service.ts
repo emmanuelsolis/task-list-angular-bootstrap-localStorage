9f8710878373f49e0c35a2f6158deae10820e814
Emmanuel Solis <emma.ruso@outlook.com>
2024-11-07 21:38:28 -0600

    Logramos guardar y eliminar datos en el localStorage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,25 +5,38 @@ import { Task } from '../models/Task';
   providedIn: 'root'
 })
 export class TaskService {
-
-  tasks: Task[];
-
+  tasks: Task[] = [];
   constructor() { 
     this.tasks = [
-      {title: "Crear Contenido", description: "Escribir contenido para el sitio web"},
-      {title: "Crear Pagina", description: "Crear una pagina web"},
+      // {title: "Crear Contenido", description: "Escribir contenido para el sitio web"},
+      // {title: "Crear Pagina", description: "Crear una pagina web"},
     ];
   }
   getTasks(){
-    return this.tasks;
+    if(localStorage.getItem('tasks') === null) {
+      return this.tasks;
+    }else{
+      this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+      return this.tasks;
   }
-  addTask(task: Task){
+  }
+
+  addTask(task: Task) {
     this.tasks.push(task);
+    if (localStorage.getItem('tasks') === null) {
+      this.tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+      this.tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
   }
   deleteTask(task:Task){
     for(let i=0; i < this.tasks.length; i++){
       if(task === this.tasks[i]){
         this.tasks.splice(i, 1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
     }
   }
